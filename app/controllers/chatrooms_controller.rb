c3722a3ccb3ce445a8bd694b51e73524bb01ae40
Vojtech Matous <vojtech.matous@gmail.com>
2021-06-02 17:32:43 +0200

    implementing the chat

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -2,5 +2,26 @@ class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
+    raise
   end
+
+  # create action, user clicks on the buttom, read the params and create the chatroom
+
+  def create
+    # @chatroom = Chatroom.create(gift_request_id: current_user.)
+  end
+
+
+  # def create
+  #   @giftrequest = GiftRequest.new(giftrequest_params)
+  #   @giftrequest.requester = current_user
+  #   if @giftrequest.save
+  #     flash[:success] = "GiftRequest successfully created"
+  #     redirect_to my_requests_gift_requests_path
+  #   else
+  #     flash[:error] = "Something went wrong"
+  #     render 'new'
+  #   end
+  # end
+
 end
--- app/models/gift_request.rb
+++ app/models/gift_request.rb
@@ -2,6 +2,8 @@ class GiftRequest < ApplicationRecord
   belongs_to :requester, foreign_key: :requester_id, class_name: 'User'
   belongs_to :shopper, foreign_key: :shopper_id, class_name: 'User', optional: true
 
+  has_one :chatroom # every gift_request has only one chatroom -> gives me access
+
   validates :recipient_name, presence: true
   validates :recipient_address, presence: true
   validates :products, presence: true
