372eda76e3e2afa8f59f9885c5004a8e8c1c0fa3
Emily Hoult <emily@emilyhoult.com>
2021-06-07 18:17:30 +0200

    add sidebar style to current selection

---
+++ app/javascript/custom/sidebar.js
@@ -0,0 +1,14 @@
+
+//   const sidebar = () => {
+//       document.querySelectorAll('.nav-link').forEach( (icon) => {
+//         icon.addEventListener("click", (event) => {
+//           console.log(event.currentTarget);
+//           // event.preventDefault();
+//           const currentIcon = event.currentTarget;
+//           currentIcon.classList.add('current-icon');
+//         });
+//       });
+//   };
+
+// export { sidebar };
+
