4738e0f64ef53fc83e20130a8808698eee408acd
Vojtech Matous <vojtech.matous@gmail.com>
2021-06-08 16:03:56 +0200

    some comments eraised

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,8 +1,5 @@
 class PaymentsController < ApplicationController
   def new
     @giftrequest = GiftRequest.find(params[:gift_request_id])
-    #raise
-    #@giftrequest = current_user.gift_requests.where(state: 'pending').find(params[:gift_request_id])
   end
-
 end
