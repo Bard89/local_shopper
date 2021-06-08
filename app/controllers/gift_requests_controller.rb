55c7d41ee6bda0e3ac1a5ebd2708e306e7ba95f5
Vojtech Matous <vojtech.matous@gmail.com>
2021-06-08 14:53:09 +0200

    payment stripe site fixed

--- app/controllers/gift_requests_controller.rb
+++ app/controllers/gift_requests_controller.rb
@@ -33,7 +33,7 @@ class GiftRequestsController < ApplicationController
     )
 
     @giftrequest.update(checkout_session_id: session.id)
-    redirect_to new_order_payment_path(order)
+    redirect_to new_gift_request_payment_path(@giftrequest)
   end
 
 
--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,5 +1,7 @@
 class PaymentsController < ApplicationController
   def new
-    @giftrequest = current_user.orders.where(state: 'pending').find(params[:gift_request_id])
+    @giftrequest = GiftRequest.find(params[:gift_request_id])
+    #@giftrequest = current_user.gift_requests.where(state: 'pending').find(params[:gift_request_id])
   end
+
 end
