745e4d0d9e75b944382b4f2cd96cf31b738aec37
Hannah Eichelsdoerfer <hannaheichelsdoerfer@gmail.com>
2021-06-08 17:23:00 +0200

    Fix Show

--- app/controllers/gift_requests_controller.rb
+++ app/controllers/gift_requests_controller.rb
@@ -11,28 +11,6 @@ class GiftRequestsController < ApplicationController
     # order  = Order.create!(teddy: teddy, teddy_sku: teddy.sku, amount: teddy.price, state: 'pending', user: current_user)
     # raise
     # this works with the stripe, sends some data to stripe, so the stripe knows what the user is paying for ... some of it will be on the receipt then
-
-    session = Stripe::Checkout::Session.create(
-      payment_method_types: ['card'],
-      # we are passing an array of hashes, each hash represents one order/ one gift in out case
-      # do some if statemnt to check id there are more than one gifts
-      line_items: [{
-        name: "gift", # maybe not rigt for the financial sevices, but let's not list the products
-        # images: [gift_request.photo_url],
-        amount: @giftrequest.price_cents, # this can't be 0 here
-        currency: 'eur',
-        description: @giftrequest.comment,
-        quantity: 1 # we will always have 1, we don't have the option to change it, user would have to pick the same item multiple times
-      }],
-      # problem here is that we only have the price as a whole
-      # {name: @giftrequest.product2,},
-      # {}],
-      success_url: gift_request_url(@giftrequest), # where to go after doing the payment
-      cancel_url: gift_request_url(@giftrequest) # we wanna go to the showpage of the order
-    )
-
-    @giftrequest.update(checkout_session_id: session.id)
-    #redirect_to new_gift_request_payment_path(@giftrequest)
   end
 
   def new
@@ -97,13 +75,7 @@ class GiftRequestsController < ApplicationController
     redirect_to shopper_dashboard_path, notice: "Status for #{@giftrequest.recipient_name}'s gift updated to #{@giftrequest.status}"
   end
 
-  def gift_price
-    @giftrequest = GiftRequest.find(params[:id])
-    @giftrequest.update(price_cents: 10000)
-    @giftrequest.update(status: "purchased")
-    redirect_to gift_request_path(@giftrequest)
-  end
-
+  
   def accept
     @giftrequest = GiftRequest.find(params[:id])
     @giftrequest.update(status: params[:status])
@@ -111,6 +83,40 @@ class GiftRequestsController < ApplicationController
     @chatroom = Chatroom.create(gift_request_id: @giftrequest.id) # I create the chatroom on accepting the request by the other user (need 2 users for that)
     # was not working before as it was Chatroom.new instead of Chatroom.create
     redirect_to gift_request_path(@giftrequest), notice: "You've succesfully taken on #{@giftrequest.requester.first_name}'s' gift request!"
+  end
+  
+  def gift_price
+    @giftrequest = GiftRequest.find(params[:id])
+    @giftrequest.update(price_cents: 10000)
+    @giftrequest.update(status: "purchased")
+    redirect_to gift_request_path(@giftrequest)
+  end
+
+  def pay
+    @giftrequest = GiftRequest.find(params[:id])
+    if @giftrequest.shopper && @giftrequest.price_cents
+      session = Stripe::Checkout::Session.create(
+        payment_method_types: ['card'],
+        # we are passing an array of hashes, each hash represents one order/ one gift in out case
+        # do some if statemnt to check id there are more than one gifts
+        line_items: [{
+          name: "gift", # maybe not rigt for the financial sevices, but let's not list the products
+          # images: [gift_request.photo_url],
+          amount: @giftrequest.price_cents, # this can't be 0 here
+          currency: 'eur',
+          description: @giftrequest.comment,
+          quantity: 1 # we will always have 1, we don't have the option to change it, user would have to pick the same item multiple times
+        }],
+        # problem here is that we only have the price as a whole
+        # {name: @giftrequest.product2,},
+        # {}],
+        success_url: gift_request_url(@giftrequest), # where to go after doing the payment
+        cancel_url: gift_request_url(@giftrequest) # we wanna go to the showpage of the order
+      )
+      @giftrequest.update(checkout_session_id: session.id)
+      #redirect_to new_gift_request_payment_path(@giftrequest)
+      @giftrequest.update(status: "paid")
+    end
   end
 
   private
