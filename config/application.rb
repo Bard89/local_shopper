9a478f93c6a103c4b1c88820fad06861946695c6
Vojtech Matous <vojtech.matous@gmail.com>
2021-06-09 17:56:59 +0200

    timezone changed

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,9 @@ module LocalShopper
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    #set timezone to local
+    config.time_zone = "Europe/Berlin"
+    config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
