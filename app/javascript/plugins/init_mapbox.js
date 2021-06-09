692228c95299b85409f6664c34523cfc5cadce7d
maisaroq <mais209@gmail.com>
2021-06-09 11:29:26 +0200

    map progress

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,5 +1,8 @@
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
@@ -17,6 +20,7 @@ const initMapbox = () => {
         .addTo(map);
   });
     fitMapToMarkers(map, markers);
+    addMarkersToMap(map, markers);
   }
 };
 
@@ -25,6 +29,22 @@ const fitMapToMarkers = (map, markers) => {
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
   map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
 };
+
+const addMarkersToMap = (map, markers) => {
+  markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.info_window);
+    new mapboxgl.Marker()
+      .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup) // add this
+      .addTo(map);
+    });
+
+  if (mapElement) {
+      map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl}));
+  }
+};
+
 
 
 export { initMapbox };
