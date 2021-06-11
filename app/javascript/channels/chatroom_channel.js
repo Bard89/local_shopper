deb013f8930e692ff288a84e9035b146d0d351d6
Hannah Eichelsdoerfer <hannaheichelsdoerfer@gmail.com>
2021-06-11 12:14:23 +0200

    Functioning Chatroom Message Styling

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -7,7 +7,20 @@ const initChatroomCable = () => {
 
     consumer.subscriptions.create({ channel: "ChatroomChannel", id: id }, {
       received(data) {
-        messagesContainer.insertAdjacentHTML('beforeend', data);
+        // console.log(data);
+        const element = document.getElementById("user-id");
+        // console.log(element);
+        const user_id = Number(element.getAttribute("data-user-id"));
+  
+        let html;
+        if (user_id === data.message.user_id) {
+          html = data.current_user_messages;
+          // console.log(html);
+        } else {
+          html = data.other_user_messages;
+          // console.log(html);
+        }
+        messagesContainer.insertAdjacentHTML('beforeend', html);
       }
     });
   }
