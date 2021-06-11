855593685bfe85acdb1abd2ff8062dffa0afe964
maisaroq <mais209@gmail.com>
2021-06-11 12:21:00 +0200

    color maps

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -11,7 +11,7 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/maisaroq/ckps6f2vy01z517qf0l0smyx7'
     });
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
